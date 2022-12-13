aced9cd9bbce59923ed6d6d43faa2df9a432eb83
bzuist <118041460+bzuist@users.noreply.github.com>
2022-12-13 21:27:46 +0300

    Add files via upload

---
+++ record.h
@@ -0,0 +1,21 @@
+#ifndef RECORD_H
+#define RECORD_H
+
+#include <QMainWindow>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private:
+    Ui::MainWindow *ui;
+};
+#endif // RECORD_H
